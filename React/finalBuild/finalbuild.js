b362b9f5739258d7c29e8e316ebcd12e9f593fc4
rajravikum <Rajarajan.Ravikumar@csscorp.com>
2021-12-07 10:02:11 +0530

    Errors Fixed

--- React/finalBuild/finalbuild.js
+++ React/finalBuild/finalbuild.js
@@ -1,1 +1,3 @@
-(()=>{"use strict";console.log("Greetings and call from the App.js"),console.log("How are doing"),console.log("Iam working in CSS-CORP"),console.log(app.a)})();
+(() => {
+  console.log('Greetings and call from the App.js'), console.log('How are doing'), console.log('Iam working in CSS-CORP'), console.log(app.a);
+})();
