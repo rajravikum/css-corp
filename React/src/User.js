fb2fd658aa42ee7af1a9c1ba7bcf74a4e1c7dbf9
rajravikum <Rajarajan.Ravikumar@csscorp.com>
2021-12-08 00:21:07 +0530

    React task caompleted

---
+++ React/src/User.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Userinfo extends Component {
+  static propTypes = {
+    firstName: PropTypes.string.isRequired,
+    lastName: PropTypes.string.isRequired,
+  };
+
+    test() {}
+    render() {
+    const { firstName, lastName } = this.props;
+    return (
+     <div>
+          <h1>{firstName} {lastName}</h1>
+      </div>
+    );
+  }
+}
+export default User;
